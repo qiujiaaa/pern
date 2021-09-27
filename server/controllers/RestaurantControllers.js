e8cc66c34d71512b06ff1e2286e3ea20f633a005
qiujiaaa <ooiqiujia@hotmail.com>
2021-09-27 16:15:10 +0800

    set up all sql commands for the routes

--- server/controllers/RestaurantControllers.js
+++ server/controllers/RestaurantControllers.js
@@ -1,37 +1,76 @@
 const db = require('../database');
 
 const getAllRestaurants = async (req, res) => {
-  const results = await db.query('select * from restaurants');
-  console.log(results);
-  res.status(200).json({
-    message: 'These are the restaurants',
-  });
+  try {
+    const results = await db.query('select * from restaurants');
+    res.status(200).json({
+      status: 'success',
+      results: results.rows.length,
+      data: { restaurants: results.rows },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const getRestaurant = (req, res) => {
-  res.status(200).json({
-    message: `Get ${req.params.id}`,
-  });
+const getRestaurant = async (req, res) => {
+  try {
+    const result = await db.query('select * from restaurants where id = $1', [
+      req.params.id,
+    ]);
+    res.status(200).json({
+      status: 'success',
+      results: result.rows.length,
+      data: { restaurants: result.rows },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const addRestaurant = (req, res) => {
-  res.status(201).json({
-    message: `Add ${req.body}`,
-  });
-  console.log(req.body);
+const addRestaurant = async (req, res) => {
+  try {
+    const result = await db.query(
+      'insert into restaurants (name, location, price_range) values ($1, $2, $3) returning *',
+      [req.body.name, req.body.location, req.body.price_range]
+    );
+    res.status(200).json({
+      status: 'success',
+      results: result.rows.length,
+      data: { restaurants: result.rows[0] },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const editRestaurant = (req, res) => {
-  res.status(200).json({
-    message: `Edit ${req.body}`,
-  });
-  console.log(req.body);
+const editRestaurant = async (req, res) => {
+  try {
+    const result = await db.query(
+      'update restaurants set name = $1, location = $2, price_range = $3 where id = $4 returning *',
+      [req.body.name, req.body.location, req.body.price_range, req.params.id]
+    );
+    res.status(200).json({
+      status: 'success',
+      results: result.rows.length,
+      data: { restaurants: result.rows[0] },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const deleteRestaurant = (req, res) => {
-  res.status(204).json({
-    message: `Restaurant deleted.`,
-  });
+const deleteRestaurant = async (req, res) => {
+  try {
+    const result = await db.query('delete from restaurants where id = $1', [
+      req.params.id,
+    ]);
+    res.status(200).json({
+      status: 'success',
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
